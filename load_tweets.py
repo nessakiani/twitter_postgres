244f13063f19d6c03b4e03653c44174e967498af
nessakiani <nessakiani@gmail.com>
2023-03-29 18:39:34 -0700

    fixed indentation

--- load_tweets.py
+++ load_tweets.py
@@ -91,7 +91,7 @@ def insert_tweet(connection,tweet):
         # insert tweet within a transaction;
         # this ensures that a tweet does not get "partially" loaded
     
-    with connection.begin() as trans:
+        with connection.begin() as trans:
 
             # skip tweet if it's already inserted
             
